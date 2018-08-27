840483f52c1b04b5a513528fa538949530c05ae3
Hendrik Sollich <hendrik@hoodie.de>
2018-08-27 17:18:28 +0200

    Fifth commit

--- src/main.js
+++ src/main.js
@@ -3,20 +3,18 @@ import App from './App.html';
 const app = new App({
 	target: document.body,
 	data: {
-		name: 'TapTap',
-
 		selectedTap: null,
 		newTapName: null,
 
 		taps: {
-			hendrik: [
-				{ name: 'coffee', price: 0.80 },
-				{ name: 'mate', price: 1.30 },
-				{ name: 'PremiumCola', price: 1.30 },
-			],
-			kilian: [
-				{ name: 'Kolle Mate', price: 1.30 },
-			]
+			// hendrik: [
+			// 	{ name: 'coffee', price: 0.80 },
+			// 	{ name: 'mate', price: 1.30 },
+			// 	{ name: 'PremiumCola', price: 1.30 },
+			// ],
+			// kilian: [
+			// 	{ name: 'Kolle Mate', price: 1.30 },
+			// ]
 		},
 
 		catalogue: [
