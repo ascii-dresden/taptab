113ea9685b6d3b9bdf7a87d94f0f434ba8ce1595
Hendrik Sollich <hendrik@hoodie.de>
2018-08-27 15:05:05 +0200

    Second commit

--- src/main.js
+++ src/main.js
@@ -12,11 +12,24 @@ const app = new App({
 			hendrik: [
 				{ name: 'coffee', price: 0.80 },
 				{ name: 'mate', price: 1.30 },
+				{ name: 'PremiumCola', price: 1.30 },
 			],
 			kilian: [
 				{ name: 'Kolle Mate', price: 1.30 },
 			]
-		}
+		},
+
+		catalogue: [
+			 { name: "Kaffee", price: .80 },
+			 { name: 'Club Mate', price: 1.30 },
+			 { name: 'Kolle Mate', price: 1.30 },
+			 { name: 'Snickers', price: .60 },
+		],
+
+		newCatalogueItem: {
+			name: null,
+			price: null
+		},
 
 	}
 });
